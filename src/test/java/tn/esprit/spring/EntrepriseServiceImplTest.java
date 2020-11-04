1e752af80e76bcd51bb588554257e49b26830d02
Sbai Ahmed <ahmed.sbai@esprit.tn>
2020-11-04 12:48:23 +0100

    last commit

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -2,10 +2,12 @@ package tn.esprit.spring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import tn.esprit.log4j.AffichageLog;
 import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.services.IDepartementService;
@@ -13,6 +15,8 @@ import tn.esprit.spring.services.IEntrepriseService;
 
 @SpringBootTest
 public class EntrepriseServiceImplTest {
+	
+	private static final Logger l = Logger.getLogger(EntrepriseServiceImplTest.class);
 
 	@Autowired
 	IEntrepriseService es ;
@@ -24,9 +28,25 @@ public class EntrepriseServiceImplTest {
 	@Test
 	void ajouterEntrepriseTest()
 	{
+try {
+			
+			// DEBUG / INFO / WARN / ERROR  
+			
+			l.info("In getAllPrducts() : ");
+			
+			l.debug("Je vais  lancer le test.");
+			// ....
+			l.debug("Je viens de lancer le test " );
+			l.debug("Je viens de finir le test."); 
+
+			l.info("Out getAllPrducts() without errors." ); // after retruning 
+		} catch (Exception e) {
+			l.error("Erreur dans getAllPrducts() : " + e); // after throwing 
+		}
+
 		Entreprise ent = new Entreprise("ggg","gggg");
 		ent.setId(2);
-		assertEquals(2,  es.ajouterEntreprise(ent));
+		assertEquals(3,  es.ajouterEntreprise(ent));
 	}
 	
 }
